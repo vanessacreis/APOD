aa365512fa125b735df729de4ee1784b98573e35
vanessacreis <vanessacreis@live.com>
2021-12-13 16:34:03 -0300

    Alteração na estilização

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $("#button").click(function () {
     url: `https://api.nasa.gov/planetary/apod?api_key=p4VIf5SgsXlfNnBBfMfSJ77eemNlw1ek6m2amrZy&date=${data}`,
     type: "GET",
     success: function (apod) {
-      console.log(apod);
+      $(".cx2").css("display", "flex");
       $("#foto").attr("src", apod.url);
       $("#titulo").text(apod.title);
       $("#texto").text(apod.explanation);
